89df3576a6415bf0168ccabec0da5278baa1d640
ErikGa25 <jerik67@gmail.com>
2021-09-02 19:21:32 -0500

    se agregaron archivos iniciales

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,5 @@
+// pregunta al usuario su nombre
+const nombre = prompt('¿Cual es tu nombre?');
+
+// Toma el nombre y lo muestra en pantalla
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JavaScript Moderno`;
