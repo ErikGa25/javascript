bfb9ba4fa9801cd2477d9964c1c5ae10c8a8e6d4
ErikGa25 <jerik67@gmail.com>
2021-09-04 18:11:35 -0500

    diferentes operadores en js

---
+++ 05-Operadores/js/02-app.js
@@ -0,0 +1,25 @@
+const num1 = 20;
+const num2 = "20";
+const num3 = 30;
+
+// si 2 números son iguales
+
+console.log(num1 == num3);
+console.log(num1 == num2);
+
+
+//comparador estricto
+console.log(num1 === num2);
+console.log(num1 === parseInt(num2)); //convertimos
+console.log(typeof num1);
+console.log(typeof num2);
+
+
+// diferentes
+const pass1 = 'admin';
+const pass2 = 'Admin';
+
+console.log(pass1 != pass2);
+console.log(num1 != num2);
+console.log(num1 !== num2);
+
---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,11 @@
+//undefined
+let numero_1;
+
+console.log(numero_1);
+
+// null
+let numero_2 = null;
+
+console.log(numero_2);
+
+console.log(numero_2 === numero_1);
