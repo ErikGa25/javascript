a354761e74a775bd8945ce7d886839f7197123fa
ErikGa25 <jerik67@gmail.com>
2021-09-07 12:26:56 -0500

    estructuras if, else, ternario, switch

---
+++ 10-Estructuras/js/03-app.js
@@ -0,0 +1,18 @@
+// Veamos como utilizar los operadores de mayor que o menor que...
+
+
+let dinero = 500;
+let totalCarrito = 300;
+
+if( dinero > totalCarrito ) {
+    console.log('Pago Correcto');
+} else {
+    console.log('Fondos Insuficientes');
+}
+
+// También puede ser sin llaves
+
+if( dinero > totalCarrito )
+    console.log('Pago Correcto');
+else
+    console.log('Fondos Insuficientes');
