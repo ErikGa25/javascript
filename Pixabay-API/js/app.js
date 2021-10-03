af426cd2eaf88ab41ade8c6b8c607c73409d8467
ErikGa25 <jerik67@gmail.com>
2021-10-03 16:49:52 -0500

    se integro asyn await con el API

--- Pixabay-API/js/app.js
+++ Pixabay-API/js/app.js
@@ -44,19 +44,29 @@ function mostrarAlerta(mensaje) {
     }
 }
 
-function buscarImagenes() {
+async function buscarImagenes() {
     // tomamos el valor de lo que el usuario escribio en el input
     const termino = document.querySelector('#termino').value;
     const key = ''; // API key de Pixabay <----------------------------->
     const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
 
-    fetch(url)
+    // código anterior
+    /*fetch(url)
         .then(respuesta => respuesta.json())
         .then(resultado => {
             // calculamos el total de paginas
             totalPaginas = calcularPaginas(resultado.totalHits)
             mostrarImagenes(resultado.hits);
-        });
+        });*/
+
+    try {
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        totalPaginas = calcularPaginas(resultado.totalHits)
+        mostrarImagenes(resultado.hits);
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 // generador que va a registrar la cantidad de elementos de acuerdo a las paginas
